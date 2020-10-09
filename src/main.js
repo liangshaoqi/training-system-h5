c43a7a4c81de4023ee3833aa59e383e33d3070d3
junexjiao <xiongjiao@htgd.com.cn>
2020-10-09 17:04:32 +0800

    fix 学习页面

--- src/main.js
+++ src/main.js
@@ -1,15 +1,16 @@
 import Vue from 'vue'
 import router from 'src/route'
 import App from './App.vue'
-import { Field, Button, Header, Tabbar, TabItem } from 'mint-ui'
+import { Field, Button, Header, Tabbar, TabItem, Cell } from 'mint-ui'
 import 'mint-ui/lib/style.css'
 import './assets/reset.css'
 
 Vue.component(Field.name, Field)
 Vue.component(Button.name, Button)
 Vue.component(Header.name, Header)
-Vue.component(Tabbar.name, Tabbar);
-Vue.component(TabItem.name, TabItem);
+Vue.component(Tabbar.name, Tabbar)
+Vue.component(TabItem.name, TabItem)
+Vue.component(Cell.name, Cell)
 
 Vue.config.productionTip = false
 
--- src/views/learn/index.vue
+++ src/views/learn/index.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="learn-box">
-    learn
+    <mt-cell title="城市概况" is-link></mt-cell>
+    <mt-cell title="政策法规" is-link></mt-cell>
+    <mt-cell title="导游服务技能、旅游安全及突发事件应急处理" is-link></mt-cell>
+    <mt-cell title="导游带团技巧及典型案例解析及应急处理" is-link></mt-cell>
+    <mt-cell title="大运宣传" is-link></mt-cell>
   </div>
 </template>
 
@@ -14,6 +18,5 @@ export default {
 </script>
 <style lang='scss'>
   .learn-box {
-    
   }
 </style>
