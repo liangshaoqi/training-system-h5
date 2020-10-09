f3d2203887129528e5d8a6eb0d317fe7a701f60e
junexjiao <xiongjiao@htgd.com.cn>
2020-10-09 14:50:10 +0800

    feat 激活成功页面

--- src/views/registSuccess/index.vue
+++ src/views/registSuccess/index.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="regist-success-box">
-    regist-success
+    <mt-header fixed title="激活成功">
+      <router-link to="" slot="left">
+        <mt-button icon="back" @click="$router.back(-1)"></mt-button>
+      </router-link>
+    </mt-header>
+
+    <img :src="require('../../assets/regist/success.png')" alt="">
+    <div class="tips">激活成功</div>
+    <mt-button type="primary" style="width: 2rem; margin: 0 auto;" @click="$router.replace('/login')">去登陆</mt-button>
   </div>
 </template>
 
@@ -14,6 +22,16 @@ export default {
 </script>
 <style lang='scss'>
   .regist-success-box {
-    
+    padding-top: 1rem;
+    text-align: center;
+    > img {
+      width: .68rem;
+      height: .68rem;
+    }
+    > .tips {
+      font-size: .2rem;
+      color: #333;
+      line-height: .6rem;
+    }
   }
 </style>
